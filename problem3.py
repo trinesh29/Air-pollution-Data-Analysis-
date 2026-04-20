dd8a2b522f15f09e95cba47ecd39818e30e05cb8
Trinesh m <fen.flowing-0h@icloud.com>
2026-04-20 22:38:37 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,23 +1,39 @@
+# Problem 3:
+# To analyze pollution across different cities using bar and pie charts.
 
-##Correlation + Scatter
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv("dataset.csv")
 
-df['pollutant_min'] = pd.to_numeric(df['pollutant_min'], errors='coerce')
-df['pollutant_max'] = pd.to_numeric(df['pollutant_max'], errors='coerce')
 df['pollutant_avg'] = pd.to_numeric(df['pollutant_avg'], errors='coerce')
-
 df = df.dropna()
 
-# Scatter
-plt.scatter(df['pollutant_min'], df['pollutant_max'])
-plt.title("Min vs Max Pollution")
+city_data = df.groupby('city')['pollutant_avg'].mean().sort_values(ascending=False).head(10)
+
+# -------------------------------
+# Bar Chart
+# -------------------------------
+plt.figure(figsize=(8,5))
+
+bars = plt.bar(city_data.index, city_data.values)
+
+# Labels
+for bar in bars:
+    y = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, y, round(y,2), ha='center', fontsize=8)
+
+plt.xticks(rotation=45)
+plt.title("Top Cities Pollution")
+plt.xlabel("City")
+plt.ylabel("Pollution Avg")
+
 plt.show()
 
-# Heatmap
-sns.heatmap(df[['pollutant_min','pollutant_max','pollutant_avg']].corr(), annot=True)
-plt.title("Correlation Heatmap")
+# -------------------------------
+# Pie Chart
+# -------------------------------
+city_data.plot(kind='pie', autopct='%1.1f%%')
+plt.title("City Pollution Share")
+plt.ylabel("")
 plt.show()
