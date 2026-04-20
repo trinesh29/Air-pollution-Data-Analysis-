02f3567228206c6400a389c4f1f1fe0fa8743409
Trinesh m <fen.flowing-0h@icloud.com>
2026-04-20 22:38:18 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,24 +1,39 @@
-# Distribution + Outliers
+# Problem 2:
+# To analyze the distribution of pollution values,
+# handle missing data, and visualize using histogram and boxplot.
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv("dataset.csv")
 
-df['pollutant_avg'] = pd.to_numeric(df['pollutant_avg'], errors='coerce')
-df = df.dropna()
+data = df[['pollutant_avg']]
+
+# -------------------------------
+# Missing Values
+# -------------------------------
+print("Missing Values:\n", data.isnull().sum())
+
+# Fill missing values
+data = data.fillna(data.mean(numeric_only=True))
+
+# -------------------------------
+# Visualization
+# -------------------------------
+plt.figure(figsize=(10,4))
 
 # Histogram
-plt.hist(df['pollutant_avg'], bins=30)
+plt.subplot(1,2,1)
+plt.hist(data['pollutant_avg'], bins=30)
 plt.title("Pollution Distribution")
-plt.show()
+plt.xlabel("Pollution Avg")
+plt.ylabel("Frequency")
+plt.grid()
 
 # Boxplot
-plt.boxplot(df['pollutant_avg'])
-plt.title("Outliers Detection")
-plt.show()
+plt.subplot(1,2,2)
+plt.boxplot(data['pollutant_avg'])
+plt.title("Pollution Boxplot")
 
-# Violin Plot
-sns.violinplot(y=df['pollutant_avg'])
-plt.title("Distribution Shape")
+plt.tight_layout()
 plt.show()
